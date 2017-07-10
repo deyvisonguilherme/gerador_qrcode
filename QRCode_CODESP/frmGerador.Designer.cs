c4c0bd37992c3af0f18f4d18e54dc512db6c11e5
Deyvison Guilherme <deyvison.guilherme@live.com>
2017-07-10 16:05:55 -0300

    Initial Commit

---
+++ QRCode_CODESP/frmGerador.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace QRCode_CODESP
+{
+    partial class frmGerador
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picQRCode = new System.Windows.Forms.PictureBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picQRCode
+            // 
+            this.picQRCode.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.picQRCode.Location = new System.Drawing.Point(334, 27);
+            this.picQRCode.Name = "picQRCode";
+            this.picQRCode.Size = new System.Drawing.Size(102, 100);
+            this.picQRCode.TabIndex = 0;
+            this.picQRCode.TabStop = false;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(12, 27);
+            this.txtInfo.MaxLength = 45;
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(316, 100);
+            this.txtInfo.TabIndex = 1;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(12, 133);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGerar.TabIndex = 2;
+            this.btnGerar.Text = "&Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Informações:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(331, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Imagem QRCode:";
+            // 
+            // frmGerador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 174);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.picQRCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmGerador";
+            this.Text = "Gerador de QRCode";
+            ((System.ComponentModel.ISupportInitialize)(this.picQRCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picQRCode;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ QRCode_CODESP/frmGerador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace QRCode_CODESP
+{
+    public partial class frmGerador : Form
+    {
+        public frmGerador()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            if (txtInfo.Text == String.Empty)
+                MessageBox.Show("O campo informações é obrigatório", "Administrador");
+            else
+                picQRCode.Image = QRCode(200, 200, txtInfo.Text);
+        }
+
+        private Bitmap QRCode(int v1, int v2, string text)
+        {
+            try
+            {
+                ZXing.BarcodeWriter bw = new ZXing.BarcodeWriter();
+                ZXing.Common.EncodingOptions encOptions = new ZXing.Common.EncodingOptions()
+                {
+                    Height = v1,
+                    Width = v2,
+                    Margin = 0
+                };
+                bw.Format = ZXing.BarcodeFormat.QR_CODE;
+                Bitmap imagem = new Bitmap(bw.Write(text));
+                return imagem;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Administrador");
+                throw;
+            }
+        }
+    }
+}
